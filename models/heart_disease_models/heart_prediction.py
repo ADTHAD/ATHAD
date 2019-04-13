ce351c9f941edb77129514b6875db15588b78296
Gauravsahadev <gauravsahadeo6@gmail.com>
2019-04-13 21:31:33 +0530

    clean up

---
+++ models/heart_disease_models/heart_prediction.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 11 23:13:13 2019
+
+@author: gaurav
+"""
+from sklearn.externals import joblib
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv("heart.csv")
+df.head()
+
+a = pd.get_dummies(df['cp'], prefix = "cp")
+b = pd.get_dummies(df['thal'], prefix = "thal")
+c = pd.get_dummies(df['slope'], prefix = "slope")
+
+frames = [df, a, b, c]
+df =pd.concat(frames,axis=1)
+df = df.drop(columns = ['cp', 'thal', 'slope'])
+df.head()
+
+y = df.target.values
+x_data = df.drop(['target'], axis = 1)
+x = (x_data - np.min(x_data)) / (np.max(x_data) - np.min(x_data)).values
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.2,random_state=0)
+
+#transpose matrices
+x_train = x_train.T
+y_train = y_train.T
+x_test = x_test.T
+y_test = y_test.T
+
+#Support vector machine
+from sklearn.svm import SVC
+svm = SVC(random_state = 1)
+svm.fit(x_train.T, y_train.T)
+model = SVC(kernel='linear').fit(x_train.T,y_train.T)
+joblib.dump(model, 'heart_svm_model.pkl', compress=True)
+print(svm.predict(x_test.T))
+print("Test Accuracy of SVM Algorithm: {:.2f}%".format(svm.score(x_test.T,y_test.T)*100))
+
+# Random Forest Classification
+from sklearn.ensemble import RandomForestClassifier
+rf = RandomForestClassifier(n_estimators = 1000, random_state = 1)
+rf.fit(x_train.T, y_train.T)
+print("Random Forest Algorithm Accuracy Score : {:.2f}%".format(rf.score(x_test.T,y_test.T)*100))
